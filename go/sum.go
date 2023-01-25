79fe7dc0452616557431bf881676d88b9c8ea566
Poncion <ilfk@hotmail.com>
2023-01-25 00:25:59 -0300

    Trabalhando com cobertura de código

--- go/sum.go
+++ go/sum.go
@@ -3,9 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(sum(2,2))
+	fmt.Println(sum(2, 2))
 }
 
 func sum(a int, b int) int {
 	return a + b
+}
+
+func sub(a int, b int) int {
+	return a - b
+}
+
+func times(a int, b int) int {
+	return a * b
+}
+
+func sumX(a int, b int) int {
+	return a + b + a
 }
